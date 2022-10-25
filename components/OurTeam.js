04bf414daacf0d1f1e62a012fdc6b07de6cbcfcb
Chris Tomshack <ctomshack15@gmail.com>
2022-10-25 14:55:59 -0400

    added LinkedIn links for team members

--- components/OurTeam.js
+++ components/OurTeam.js
@@ -21,8 +21,7 @@ const people = [
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
     bio: 'Allison is a people person and is always interested in knowing more about an individual’s passions and goals. As our Director of Operations, she also possesses strong attention to detail and is great at bringing people together to discuss how to turn their practice into a well-oiled machine by implementing systems like Apex Longevity. These attributes make her the perfect person to work with prospective and current providers.',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+    linkedinUrl: 'https://www.linkedin.com/in/allison-danielson-7149a293/',
     alt: 'Headshot of the Director of Operations'
   },
     {
@@ -31,8 +30,8 @@ const people = [
       imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
       bio: 'Dr. Tomshack is defined by his relentless desire to discover better ways to both help patients achieve phenomenal health while assisting doctors to achieve their dreams by first creating their ultimate practice. Dr. Tomshack realized there was a huge need for chiropractic offices to legally offer discounted care and thus created Apex Longevity. Dr. Tomshack has degrees in chiropractic and business and has studied business at the graduate level, making him a unicorn among chiropractors. His passion is helping doctors create their dream lifestyle by building successful practices delivering exceptional care.',
-      twitterUrl: '#',
-      linkedinUrl: '#',
+
+      linkedinUrl: 'https://www.linkedin.com/in/chris-tomshack-a25607a/',
       alt: 'Headshot of the Founder & CEO'
     },
     {
@@ -41,8 +40,8 @@ const people = [
       imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
       bio: `Lisa has been with Apex Longevity since its inception in 2017.  Lisa's positive energy is contagious and she is an integral component that helps make the Apex culture the very best.  She is a CT graduate from Palmer college of Chiropractic and loves to bike, walk, travel, landscape and enjoy precious time with her family and friends.`,
-      twitterUrl: '#',
-      linkedinUrl: '#',
+
+      linkedinUrl: 'https://www.linkedin.com/in/lisa-tomshack-69151785/',
       alt: 'Headshot of the Executive Vice President & HR'
     },
     {
@@ -51,8 +50,8 @@ const people = [
       imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
       bio: 'Kristen Wallace spearheads the financial strategy at Apex Longevity as a Corporate Controller. With a background in corporate financing and financial reporting systems, Kristen can manage daily accounting activity while setting a course for long-term growth. She has served as a valued accounting and financial advisor at Apex Longevity since 2017.',
-      twitterUrl: '#',
-      linkedinUrl: '#',
+
+      linkedinUrl: 'https://www.linkedin.com/in/kristen-wallace-694a1a/',
       alt: 'Headshot of the Coporate Controller'
     },
   ]
@@ -91,17 +90,9 @@ const people = [
                             <p className="text-apexG">{person.role}</p>
                           </div>
                           <div className="text-lg">
-                            <p className="text-gray-500">{person.bio}</p>
+                            <p className="text-gray-500 font-light">{person.bio}</p>
                           </div>
                           <ul role="list" className="flex space-x-5">
-                            <li>
-                              <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
-                                <span className="sr-only">Twitter</span>
-                                <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                  <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                                </svg>
-                              </a>
-                            </li>
                             <li>
                               <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
                                 <span className="sr-only">LinkedIn</span>
